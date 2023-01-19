86730505991a90740711dd499b83a01841d2a4cf
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2023-01-19 14:44:34 +0200

    Ready to deploy backend

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -150,7 +150,7 @@ const HomeScreen = () => {
           <Player
             track={chosenTrack}
             albumImage={songImage}
-            source={`http://localhost:5002/api/${query} audio`}
+            source={`https://bentify-api.onrender.com/api/${query} audio`}
             isSongClicked={songClicked}
           />
         </div>
