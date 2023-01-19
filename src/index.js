22c26a9ec3247ddf6cc423fb8be4bcfa0a95203e
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2023-01-19 14:05:41 +0200

    Ready to deploy backend

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-
-process.env.NODE.ENV === "production" && disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
